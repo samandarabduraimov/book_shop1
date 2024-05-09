cb2b98c547a996a823e37407febd7acae6de0226
samandarabduraimov <samandarabduraimov541@gmail.com>
2024-05-09 22:01:06 +0500

    second commit

---
+++ users/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    image = models.ImageField(upload_to='users_images/', blank=True, null=True, default='default_img/user_img.png')
+
+    class Meta:
+        db_table = 'customuser'
+
+    def __str__(self):
+        return self.username
